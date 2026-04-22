db4648e54ddecd3604e2876d024fa48aa2205db9
maxred01 <maksim.pvo@gmail.com>
2026-04-22 21:06:43 +0300

    32321321

---
+++ test/test_example.py
@@ -0,0 +1,40 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect, APIRequestContext, Page
+from conftest import api_request_context
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def test_last_created_issue_should_be_on_the_server(api_request_context: APIRequestContext) -> None:
+    new_issue = api_request_context.get(url='/booking')
+    print(new_issue.ok)
+    print(new_issue.json())
+
+
+def test_run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    page = context.new_page()
+    page.goto("https://hoster.by/")
+    page.get_by_role("button", name="Принять").click()
+    page.get_by_text("Домены и хостинг").click()
+    page.get_by_text("Облачные сервисы", exact=True).click()
+    page.get_by_role("banner").get_by_text("Кибербезопасность").click()
+    page.locator("div").filter(has_text="Кибербезопасность").nth(5).click()
+    page.get_by_role("textbox", name="Введите домен или слово").click()
+    page.get_by_role("textbox", name="Введите домен или слово").fill("demodemo")
+    page.get_by_role("textbox", name="Введите домен или слово").press("Enter")
+    page.locator("#domain_by").click()
+    page.locator("#domain_of_by").click()
+    page.locator("[id=\"domain_бел\"]").click()
+    context.tracing.stop(path="trace.zip")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    test_run(playwright)
