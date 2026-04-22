eb5161df141fbcf9c45cb8f85eb9535203ab59d6
maxred01 <maksim.pvo@gmail.com>
2026-04-22 21:20:53 +0300

    32321321

--- test/test_example_ui.py
+++ test/test_example_ui.py
@@ -4,13 +4,6 @@ from conftest import api_request_context
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
-
-def test_last_created_issue_should_be_on_the_server(api_request_context: APIRequestContext) -> None:
-    new_issue = api_request_context.get(url='/booking')
-    print(new_issue.ok)
-    print(new_issue.json())
-
-
 def test_run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
